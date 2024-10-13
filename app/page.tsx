1df54512236e708006063c1b6bd597ce9c789a78
Aditya Raj <adiabhiraj141@gmail.com>
2024-10-13 13:22:51 +0530

    digi ki mang

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,16 @@ import GET_ACTIVE_ITEMS from "@/constants/subgraphQuerys";
 import { useQuery } from "@apollo/client";
 import NFTCard from "@/components/ui/NftBox";
 import Link from "next/link";
+import { useWallet } from "@/contexts/WalletProvide";
 
 export default function Home() {
 
   const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS);
+  const { isConnected } = useWallet();
   // console.log(listedNfts);
-
+  if (!isConnected) {
+    return <div>Not Connected</div>
+  }
   return (
     <div className="">
       <h1 className="font-bold text-center text-4xl ">All NFTs</h1>
